20eabe602292f908659ae0b295df38ac9b273dc1
hamed <intellercellc@gmail.com>
2023-11-05 12:36:40 -0800

    Fix dir creation

--- download_bashscripts/DownloadSD.py
+++ download_bashscripts/DownloadSD.py
@@ -1,6 +1,11 @@
+import os
 from huggingface_hub import snapshot_download
+
+
+sd_path = "models/StableDiffusion/stable-diffusion-v1-5"
+os.makedirs(sd_path, exist_ok=True)
 
 snapshot_download(repo_id="runwayml/stable-diffusion-v1-5", 
                 revision="fp16",
-                local_dir="models/StableDiffusion/stable-diffusion-v1-5"
+                local_dir=sd_path
                 )
