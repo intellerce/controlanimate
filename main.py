cfcd729c59ffb8cd32cb4799c05126d20d75fc4b
hamed <intellercellc@gmail.com>
2023-11-05 10:46:42 -0800

    Add try except

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ from scripts.vid2vid import vid2vid
 
 
 if __name__ == '__main__':
-        # try:
+        try:
                 parser = argparse.ArgumentParser()
                 parser.add_argument("--config", type=str, required=True)
                 args = parser.parse_args()
                 vid2vid(config_path=args.config)
-        # except Exception as e:
-        #         # error = str(traceback.format_exc())
-        #         print("An Error occured: ", e)
+        except Exception as e:
+                # error = str(traceback.format_exc())
+                print("An Error occured: ", e)
         
